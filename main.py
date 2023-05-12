f9e4b064e1c76e083f269e7c19bce184ace1c7c6
Brofes <46400703+Brofes1@users.noreply.github.com>
2023-05-12 13:17:17 -0400

    Update main.py
    Added some new info

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-#Variables
+#This is a comment!
 
-#Syntax
-aVariable = "thing"  #String
-aVariable = 69  #Integer
-aVariable = True  #Boolean
+#Variables
+	#Syntax
+		aVariable = "thing"		#String
+		aVariable = 69		#Integer
+		aVariable = True	#Boolean
+		aVariable = {}		#List
+		aVariable = anotherVariable		#The type of [anotherVariable]
+	#Strings
+		string1 = "aaa"
+		string2 = "bbb"
+		string3 = "ccc"
+		integer = 1234
+		
+		print(string1 + string2)	#Prints aaabbb
+		print(string2 + string3)	#Prints bbbccc
+		print(string1 + string3)	#Prints aaaccc
+		print(string1 + integer)	#RETURNS ERROR. You cannot add an integer (or any non-string type) to a string
+		print(string1 + str(integer))	#Does work. See following section for explanation of str() function
+		#str()
+			str(1234)	#Outputs "1234"
+			str(True)	#Outputs "True"
+			
+			alist = {}
+			alist['20'] = "thing1"
+			alist[20] = "thing2"
+			str(alist)	#Outputs {['20', "thing1"], [20, "thing2"]} (TODO: Don't remember exact output)
+			
